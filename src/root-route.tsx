bf1892c8187228d3a56910168c7f4f8b340f3336
cyh7834 <cyh7834@gmail.com>
2026-02-21 13:56:36 +0900

    페이지 라우팅 설정하기

---
+++ src/root-route.tsx
@@ -0,0 +1,23 @@
+import { Navigate, Route, Routes } from "react-router";
+import SignInPage from "./pages/sign-in-page";
+import SignUpPage from "./pages/sign-up-page";
+import ForgetPasswordPage from "./pages/forget-password-page";
+import IndexPage from "./pages/index-page";
+import PostDetailPage from "./pages/post-detail-page";
+import ProfileDetailPage from "./pages/profile-detail-page";
+import ResetPasswordPage from "./pages/reset-password-page";
+
+export default function RootRoute() {
+  return (
+    <Routes>
+      <Route path="/sign-in" element={<SignInPage />}></Route>
+      <Route path="/sign-up" element={<SignUpPage />}></Route>
+      <Route path="/forget-password" element={<ForgetPasswordPage />}></Route>
+      <Route path="/" element={<IndexPage />}></Route>
+      <Route path="/post/:postId" element={<PostDetailPage />}></Route>
+      <Route path="/profile/:userId" element={<ProfileDetailPage />}></Route>
+      <Route path="/reset-password" element={<ResetPasswordPage />}></Route>
+      <Route path="*" element={<Navigate to={"/"}></Navigate>}></Route>
+    </Routes>
+  );
+}
