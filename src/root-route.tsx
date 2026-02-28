b8887b3a5c160b9af8a275ef1d83c51ef12701d0
cyh7834 <cyh7834@gmail.com>
2026-02-28 09:07:43 +0900

    글로벌 레이아웃 설정

--- src/root-route.tsx
+++ src/root-route.tsx
@@ -6,18 +6,21 @@ import IndexPage from "./pages/index-page";
 import PostDetailPage from "./pages/post-detail-page";
 import ProfileDetailPage from "./pages/profile-detail-page";
 import ResetPasswordPage from "./pages/reset-password-page";
+import GlobalLayout from "./components/layout/global-layout";
 
 export default function RootRoute() {
   return (
     <Routes>
-      <Route path="/sign-in" element={<SignInPage />}></Route>
-      <Route path="/sign-up" element={<SignUpPage />}></Route>
-      <Route path="/forget-password" element={<ForgetPasswordPage />}></Route>
-      <Route path="/" element={<IndexPage />}></Route>
-      <Route path="/post/:postId" element={<PostDetailPage />}></Route>
-      <Route path="/profile/:userId" element={<ProfileDetailPage />}></Route>
-      <Route path="/reset-password" element={<ResetPasswordPage />}></Route>
-      <Route path="*" element={<Navigate to={"/"}></Navigate>}></Route>
+      <Route element={<GlobalLayout></GlobalLayout>}>
+        <Route path="/sign-in" element={<SignInPage />}></Route>
+        <Route path="/sign-up" element={<SignUpPage />}></Route>
+        <Route path="/forget-password" element={<ForgetPasswordPage />}></Route>
+        <Route path="/" element={<IndexPage />}></Route>
+        <Route path="/post/:postId" element={<PostDetailPage />}></Route>
+        <Route path="/profile/:userId" element={<ProfileDetailPage />}></Route>
+        <Route path="/reset-password" element={<ResetPasswordPage />}></Route>
+        <Route path="*" element={<Navigate to={"/"}></Navigate>}></Route>
+      </Route>
     </Routes>
   );
 }
