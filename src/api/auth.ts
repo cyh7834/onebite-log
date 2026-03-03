d075ea0e9ff6a3e0f317ccb12a0a730e3d734acb
cyh7834 <cyh7834@gmail.com>
2026-03-03 21:31:42 +0900

    로그인 구현하기

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,15 +1,35 @@
 import supabase from "@/lib/supabase";
 
-export async function signUp({email, password}: {
-    email: string;
-    password: string;
+export async function signUp({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
 }) {
-    const {data, error} = await supabase.auth.signUp({
-        email,
-        password
-    })
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
 
-    if (error) throw error;
+  if (error) throw error;
 
-    return data;
+  return data;
+}
+
+export async function signInWithPassword({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) {
+  const {data, error} = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) throw error;
+
+  return data;
 }
