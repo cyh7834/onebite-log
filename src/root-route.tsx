e716828b4187720ee5b32d31c6090bc336c1fe45
Yoonho Choi <cyh7834@gmail.com>
2026-03-08 11:14:56 +0900

    라우트 가드 구현하기

--- src/root-route.tsx
+++ src/root-route.tsx
@@ -7,18 +7,27 @@ import PostDetailPage from "./pages/post-detail-page";
 import ProfileDetailPage from "./pages/profile-detail-page";
 import ResetPasswordPage from "./pages/reset-password-page";
 import GlobalLayout from "./components/layout/global-layout";
+import GuestOnlyLayout from "./components/layout/guest-only-layout";
+import MemberOnlyLayout from "./components/layout/member-only-layout";
 
 export default function RootRoute() {
   return (
     <Routes>
       <Route element={<GlobalLayout></GlobalLayout>}>
+      <Route element={<GuestOnlyLayout></GuestOnlyLayout>}>
         <Route path="/sign-in" element={<SignInPage />}></Route>
         <Route path="/sign-up" element={<SignUpPage />}></Route>
         <Route path="/forget-password" element={<ForgetPasswordPage />}></Route>
+      </Route>
+        
+
+      <Route element={<MemberOnlyLayout></MemberOnlyLayout>}>
         <Route path="/" element={<IndexPage />}></Route>
         <Route path="/post/:postId" element={<PostDetailPage />}></Route>
         <Route path="/profile/:userId" element={<ProfileDetailPage />}></Route>
         <Route path="/reset-password" element={<ResetPasswordPage />}></Route>
+      </Route>
+
         <Route path="*" element={<Navigate to={"/"}></Navigate>}></Route>
       </Route>
     </Routes>
