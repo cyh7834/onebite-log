23cde7de399981345edb3ff06156d265e0f0d6dc
cyh7834 <cyh7834@gmail.com>
2026-03-12 21:47:06 +0900

    회원가입시 프로필 정보 자동 생성하기

--- src/provider/session-provider.tsx
+++ src/provider/session-provider.tsx
@@ -1,21 +1,32 @@
 import GlobalLoader from "@/components/global-loader";
+import { useProfileData } from "@/hooks/queries/use-profile-data";
 import supabase from "@/lib/supabase";
-import { useSetSession, useIsSessionLoaded } from "@/store/session";
+import { useSetSession, useIsSessionLoaded, useSession } from "@/store/session";
 import { useEffect } from "react";
 
-export default function SessionProvider({ children }: { children: React.ReactNode }) {
-    const setSession = useSetSession();
-    const isSessionLoaded = useIsSessionLoaded();
+export default function SessionProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = useSession();
+  const setSession = useSetSession();
+  const isSessionLoaded = useIsSessionLoaded();
 
-    useEffect(() => {
-        supabase.auth.onAuthStateChange((event, session) => {
-            setSession(session);
-        });
-    }, []);
+  const { data: profile, isLoading: isProfileLoading } = useProfileData(
+    session?.user.id,
+  );
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((event, session) => {
+      setSession(session);
+    });
+  }, []);
 
-    if (!isSessionLoaded) {
-        return <GlobalLoader></GlobalLoader>;
-    }
+  if (!isSessionLoaded) {
+    return <GlobalLoader></GlobalLoader>;
+  }
 
-    return children;
+  if (isProfileLoading) return <GlobalLoader></GlobalLoader>;
+
+  return children;
 }
