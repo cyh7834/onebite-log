0f4778c5ff92bd50eadfc87d077b3933bc9a21ee
cyh7834 <cyh7834@gmail.com>
2026-03-13 22:28:22 +0900

    포스트 추가 UI 구현하기 (modal provider createPortal 활용)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import ModalProvider from "./provider/modal-provider";
 import SessionProvider from "./provider/session-provider";
 import RootRoute from "./root-route";
 
 export default function App() {
-  return <SessionProvider>
-    <RootRoute></RootRoute>
-  </SessionProvider>;
+  return (
+    <SessionProvider>
+      <ModalProvider>
+        <RootRoute></RootRoute>
+      </ModalProvider>
+    </SessionProvider>
+  );
 }
