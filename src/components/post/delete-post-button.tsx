12d2a7c410859219eecaf7b7128eae783180d30c
cyh7834 <cyh7834@gmail.com>
2026-04-12 20:24:14 +0900

    댓글 UI 구현하기 - Post 삭제 성공 시 페이지 이동

--- src/components/post/delete-post-button.tsx
+++ src/components/post/delete-post-button.tsx
@@ -2,28 +2,45 @@ import { useOpenAlertModal } from "@/store/alert-modal";
 import { Button } from "../ui/button";
 import { toast } from "sonner";
 import { useDeletePost } from "@/hooks/mutations/post/use-delete-post";
+import { useNavigate } from "react-router";
 
-export default function DeletePostButton({id}: {id: number}) {
-    const openAlertModal = useOpenAlertModal();
-    const {mutate: deletePost, isPending: isDeletePostPending} = useDeletePost({
-        onError: (error) => {
-            toast.error("게시글 삭제에 실패했습니다. 다시 시도해주세요.", {position: "top-center"});
-        }
+export default function DeletePostButton({ id }: { id: number }) {
+  const openAlertModal = useOpenAlertModal();
+  const navigate = useNavigate();
+
+  const { mutate: deletePost, isPending: isDeletePostPending } = useDeletePost({
+    onSuccess: () => {
+      const pathname = window.location.pathname;
+
+      if (pathname.startsWith(`/post/${id}`)) {
+        navigate("/", { replace: true });
+      }
+    },
+    onError: (error) => {
+      toast.error("게시글 삭제에 실패했습니다. 다시 시도해주세요.", {
+        position: "top-center",
+      });
+    },
+  });
+
+  const handleDeleteClick = () => {
+    openAlertModal({
+      title: "게시글 삭제",
+      description: "정말로 이 게시글을 삭제하시겠습니까?",
+      onPositive: () => {
+        deletePost(id);
+      },
     });
+  };
 
-    const handleDeleteClick = () => {
-        openAlertModal({
-            title: "게시글 삭제",
-            description: "정말로 이 게시글을 삭제하시겠습니까?",
-            onPositive: () => {
-                deletePost(id);
-            }
-        });
-    }
-
-    return (
-        <Button disabled={isDeletePostPending} className="cursor-pointer" variant={"ghost"} onClick={handleDeleteClick}>
-            삭제
-          </Button>
-    )
+  return (
+    <Button
+      disabled={isDeletePostPending}
+      className="cursor-pointer"
+      variant={"ghost"}
+      onClick={handleDeleteClick}
+    >
+      삭제
+    </Button>
+  );
 }
