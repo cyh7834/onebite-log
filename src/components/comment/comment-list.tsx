03f6d0d51d03e9680dd2a7b050888bfa273b8312
cyh7834 <cyh7834@gmail.com>
2026-04-16 15:48:44 +0900

    댓글조회 기능 구현하기

--- src/components/comment/comment-list.tsx
+++ src/components/comment/comment-list.tsx
@@ -1,11 +1,23 @@
 import CommentItem from "@/components/comment/comment-item";
+import { useCommentsData } from "@/hooks/queries/use-comments-data";
+import Fallback from "../fallback";
+import Loader from "../loader";
 
-export default function CommentList() {
+export default function CommentList({ postId }: { postId: number }) {
+  const {
+    data: comments,
+    error: fetchCommentsError,
+    isPending: isFetchCommnetsPending,
+  } = useCommentsData(postId);
+
+  if (fetchCommentsError) return <Fallback />;
+  if (isFetchCommnetsPending) return <Loader />;
+
   return (
     <div className="flex flex-col gap-5">
-      <CommentItem />
-      <CommentItem />
-      <CommentItem />
+      {comments.map((comment) => (
+        <CommentItem key={comment.id} {...comment} />
+      ))}
     </div>
   );
 }
