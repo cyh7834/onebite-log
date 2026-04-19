0f9f23eccad14bba1f13abc443ad7ce44b322ca1
Yoonho Choi <cyh7834@gmail.com>
2026-04-19 11:23:15 +0900

    캐시 수정 기능 추가하기

--- src/hooks/mutations/comment/use-create-comment.ts
+++ src/hooks/mutations/comment/use-create-comment.ts
@@ -1,12 +1,27 @@
 import { createComment } from "@/api/comment";
-import type { UseMutationCallback } from "@/types";
-import { useMutation } from "@tanstack/react-query";
+import { useProfileData } from "@/hooks/queries/use-profile-data";
+import { QUERY_KEYS } from "@/lib/constants";
+import { useSession } from "@/store/session";
+import type { Comment, UseMutationCallback } from "@/types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useCreateComment(callbacks?: UseMutationCallback) {
+    const quertyClient = useQueryClient();
+    const session = useSession();
+    const {data: profile} = useProfileData(session?.user.id);
+    
     return useMutation({
         mutationFn: createComment,
-        onSuccess: () => {
+        onSuccess: (newComment) => {
             if (callbacks?.onSuccess) callbacks.onSuccess();
+
+            quertyClient.setQueryData<Comment[]>(QUERY_KEYS.comment.post(newComment.post_id), (comments) => {
+                if (!comments) throw new Error("댓글이 캐시 데이터에 보관되어있지 않습니다.");
+
+                if (!profile) throw new Error("프로필을 찾을 수 없습니다.");
+
+                return [{...newComment, author: profile}, ...comments];
+            })
         },
         onError: (error) => {
             if (callbacks?.onError) callbacks.onError(error);
